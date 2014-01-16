9e909bd177898a804415e3876eb0b8b0caeac9b3
Matvey Aksenov <matvey.aksenov@gmail.com>
2014-01-16 10:52:02 +0400

    Actually, shouldBe is still useful

--- src/Test/Hspec/Lens.hs
+++ src/Test/Hspec/Lens.hs
@@ -1,8 +1,13 @@
+-- | Module is designed to be conveniently imported instead of "Test.Hspec"
+--
+-- It reexports "Test.Hspec" without expectations (except for `shouldBe`)
+-- and "Test.Hspec.Expectations.Lens" expectations
 module Test.Hspec.Lens
   ( -- * Types
     Spec
   , Example
     -- * Setting expectations
+  , shouldBe
   , module Test.Hspec.Expectations.Lens
     -- * Defining a spec
   , describe
