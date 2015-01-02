fe7ac8d7bfc3fc1c94995f0f4efc4be62aeacce6
Matvey Aksenov <matvey.aksenov@gmail.com>
2015-01-02 17:05:38 +0000

    Nixed

---
+++ default.nix
@@ -0,0 +1,15 @@
+{ haskellPackages ? (import <nixpkgs> {}).haskellPackages }:
+
+haskellPackages.cabal.mkDerivation (self: rec {
+  pname = "hspec-expectations-lens";
+  version = "0.3.0.0";
+  src = ./.;
+  buildDepends = with haskellPackages; [ hspec hspecExpectations HUnit lens ];
+  testDepends = with haskellPackages; buildDepends ++ [ silently ];
+  meta = {
+    homepage = "http://supki.github.io/hspec-expectations-lens/";
+    description = "Hspec expectations for the lens stuff";
+    license = self.stdenv.lib.licenses.bsd3;
+    platforms = self.ghc.meta.platforms;
+  };
+})
